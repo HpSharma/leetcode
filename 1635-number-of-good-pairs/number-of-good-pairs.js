d993f8c160bde6815382ebf7e990012f60924cb2
Harpal Sharma <40227924+HpSharma@users.noreply.github.com>
2024-06-14 12:57:20 -0500

    Time: 55 ms (41.96%) | Memory: 48.9 MB (44.26%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.js
+++ 1635-number-of-good-pairs/number-of-good-pairs.js
@@ -3,13 +3,16 @@
  * @return {number}
  */
 var numIdenticalPairs = function (nums) {
-    let count = 0;
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] === nums[j]) {
-                count++;
-            }
+    const count = {};
+    let goodPairs = 0;
+
+    for (let num of nums) {
+        if (count[num]) {
+            goodPairs += count[num];
+            count[num] += 1;
+        } else {
+            count[num] = 1;
         }
     }
-    return count;
+    return goodPairs;
 };
