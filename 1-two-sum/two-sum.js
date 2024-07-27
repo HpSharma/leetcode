96893873f78e9fb7e338c4ce42196fca1aee2b3b
Harpal Sharma <40227924+HpSharma@users.noreply.github.com>
2024-07-27 00:12:58 -0500

    Time: 54 ms (86.50%) | Memory: 51.7 MB (16.24%) - LeetSync

---
+++ 1-two-sum/two-sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let map = new Map();
+
+    for(let i = 0; i < nums.length; i++) {
+        let sub = target - nums[i];
+        if (map.has(sub)) {
+            return [map.get(sub), i];
+        } 
+        map.set(nums[i], i);
+    }
+    return [];
+};
