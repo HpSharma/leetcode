3b2767ff7770b54831783854ff952ce725a41422
Harpal Sharma <40227924+HpSharma@users.noreply.github.com>
2024-07-28 07:59:17 -0500

    Time: 65 ms (50.94%) | Memory: 51.8 MB (71.17%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -4,15 +4,8 @@
  */
 var isPalindrome = function (s) {
     s = s.toLowerCase().replace(/[^a-z0-9]/g, "");
-    let i = 0;
-    let j = s.length - 1;
-    let flag = true;
-    while (i < j) {
-        if (s[i] != s[j]) {
-            flag = false;
-        }
-        i++;
-        j--;
+    for(let i=0; i<s.length/2; i++){
+        if(s[i] !== s[s.length-1-i]){return false}
     }
-    return flag;
+    return true;
 };
