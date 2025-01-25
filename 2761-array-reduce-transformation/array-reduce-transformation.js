dbde650d454bec3a8dab78b1fc9671048c097d5d
Harpal Sharma <40227924+HpSharma@users.noreply.github.com>
2025-01-24 21:28:12 -0500

    Time: 57 ms (39.65%) | Memory: 49.3 MB (56.56%) - LeetSync

--- 2761-array-reduce-transformation/array-reduce-transformation.js
+++ 2761-array-reduce-transformation/array-reduce-transformation.js
@@ -4,6 +4,11 @@
  * @param {number} init
  * @return {number}
  */
-var reduce = function(nums, fn, init) {
-    return nums.reduce(fn, init);
+var reduce = function(nums, fn, c) {
+    let value = c;
+    if (nums.length === 0) return c; 
+    for(let i = 0; i < nums.length; i++) {
+        value = fn(value, nums[i]);
+    }
+    return value;
 };
