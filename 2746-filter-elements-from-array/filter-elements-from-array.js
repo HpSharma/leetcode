65d8551c8271be0db794e4405a6dbe3cae9c2efd
Harpal Sharma <40227924+HpSharma@users.noreply.github.com>
2025-01-24 20:53:17 -0500

    Time: 53 ms (58.57%) | Memory: 48.6 MB (91.67%) - LeetSync

--- 2746-filter-elements-from-array/filter-elements-from-array.js
+++ 2746-filter-elements-from-array/filter-elements-from-array.js
@@ -4,5 +4,11 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-    return arr.filter(fn);
+    let filteredArr = [];
+    for(let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            filteredArr.push(arr[i]);
+        }
+    }
+    return filteredArr;
 };
